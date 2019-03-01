70ee575bc14a3960745f5b6c00bb2fb65fb2ef05
tbrabant3 <tyler.brabant@mymail.champlain.edu>
2019-02-28 21:56:38 -0500

    Added file io

--- Sources/PhoneNumberMnemonics/WordList.swift
+++ Sources/PhoneNumberMnemonics/WordList.swift
@@ -6,3 +6,17 @@ import Foundation
 // YOU FILL IN HERE
 // You'll want to create a mechanism here for loading and querying
 // words.txt. It's up to you how you do this. You may consider a struct.
+
+struct importQuery {
+    var fileFound = false
+    let filename = "../../words.txt"
+    do{
+        let words = try String(contentsOfFile: filename).components(separatedBy: .newlines)
+        fileFound = true
+    }
+    catch{
+        print("Something went wrong. File not found")
+        fileFound = false
+    }
+    
+}
